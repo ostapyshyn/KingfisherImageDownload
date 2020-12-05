7c1a5546a9acb0a5cf98013a58416f8e9a0252ac
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-12-05 07:45:11 +0200

    Initial Commit

---
+++ KingfisherExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KingfisherExample
+//
+//  Created by Volodymyr Ostapyshyn on 05.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
